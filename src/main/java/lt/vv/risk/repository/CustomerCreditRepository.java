5e559f63a7732c9e6397eba6236cec1aae1332f4
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2016-02-03 21:11:12 +0100

    Map.getOrDefault over Guava's Optional

--- src/main/java/lt/vv/risk/repository/CustomerCreditRepository.java
+++ src/main/java/lt/vv/risk/repository/CustomerCreditRepository.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
 
 @Repository
@@ -13,7 +12,7 @@ public class CustomerCreditRepository {
 	private final Map<String, Integer> customerCredits = Maps.newConcurrentMap();
 
 	public Integer getCustomerCredit(String emailAddress) {
-		return Optional.fromNullable(customerCredits.get(emailAddress)).or(0);
+		return customerCredits.getOrDefault(emailAddress, 0);
 	}
 
 	public void creditCustomerWithAmount(String emailAddress, Integer creditAmount) {
